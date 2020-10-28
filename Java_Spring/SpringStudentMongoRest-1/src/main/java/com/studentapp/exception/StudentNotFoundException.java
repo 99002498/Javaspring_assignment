b592f62c61259a1cf8dc588fe53cee5858120721
99002498 <71481395+99002498@users.noreply.github.com>
2020-10-28 07:13:45 +0530

    Add files via upload

---
+++ Java_Spring/SpringStudentMongoRest-1/src/main/java/com/studentapp/exception/StudentNotFoundException.java
@@ -0,0 +1,22 @@
+package com.studentapp.exception;
+
+public class StudentNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StudentNotFoundException() {
+	
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
